9b917c7c5e730e1900e15220649a7d9388fbbfb7
Nicole Liu <109707278+nliub@users.noreply.github.com>
2024-09-02 17:37:44 -0700

    finished most of edit warehouse styling

--- src/components/DetailsForm/DetailsForm.jsx
+++ src/components/DetailsForm/DetailsForm.jsx
@@ -1,44 +1,68 @@
-import { useState } from "react";
+import { useState, forwardRef } from "react";
 import "./DetailsForm.scss";
-const DetailsForm = ({ warehouesToEdit, formType, onSubmit }) => {
-  const [formData, setFormData] = useState({ ...warehouesToEdit });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
+const DetailsForm = forwardRef(
+  ({ warehouesToEdit, formType, className }, ref) => {
+    const [formData, setFormData] = useState({ ...warehouesToEdit });
 
-  const warehouseFields = [
-    { title: "Warehouse Name", value: warehouesToEdit.warehouse_name },
-    { title: "Street Address", value: warehouesToEdit.address },
-    { title: "City", value: warehouesToEdit.city },
-    { title: "Country", value: warehouesToEdit.country },
-  ];
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({ ...formData, [name]: value });
+    };
 
-  const contactFields = [
-    { title: "Contact Name", value: warehouesToEdit.contact_name },
-    { title: "Position", value: warehouesToEdit.contact_position },
-    { title: "Phone Number", value: warehouesToEdit.contact_phone },
-    { title: "Email", value: warehouesToEdit.contact_email },
-  ];
+    const warehouseFields = [
+      {
+        title: "Warehouse Name",
+        name: "warehouse_name",
+        value: formData.warehouse_name,
+      },
+      { title: "Street Address", name: "address", value: formData.address },
+      { title: "City", name: "city", value: formData.city },
+      { title: "Country", name: "country", value: formData.country },
+    ];
 
-  const fieldsToRender =
-    formType === "warehouse" ? warehouseFields : contactFields;
+    const contactFields = [
+      {
+        title: "Contact Name",
+        name: "contact_name",
+        value: formData.contact_name,
+      },
+      {
+        title: "Position",
+        name: "contact_position",
+        value: formData.contact_position,
+      },
+      {
+        title: "Phone Number",
+        name: "contact_phone",
+        value: formData.contact_phone,
+      },
+      { title: "Email", name: "contact_email", value: formData.contact_email },
+    ];
 
-  const formTitle =
-    formType === "warehouse" ? "Warehouse Details" : "Contact Details";
+    const fieldsToRender =
+      formType === "warehouse" ? warehouseFields : contactFields;
 
-  return (
-    <div className="form">
-      <h2 className="font-H2-SubHeader form__title">{formTitle}</h2>
-      {fieldsToRender.map((field, index) => (
-        <div key={index}>
-          <h3 className="font-H3-label form__field-title">{field.title}</h3>
-          <input className="form__input" defaultValue={field.value} />
-        </div>
-      ))}
-    </div>
-  );
-};
+    const formTitle =
+      formType === "warehouse" ? "Warehouse Details" : "Contact Details";
+
+    return (
+      <form ref={ref} className={`form ${className}`}>
+        <h2 className="font-H2-SubHeader form__title">{formTitle}</h2>
+        {fieldsToRender.map((field, index) => (
+          <div key={index}>
+            <h3 className="font-H3-label form__field-title">{field.title}</h3>
+            <input
+              className="form__input"
+              name={field.name}
+              defaultValue={field.value}
+              onChange={handleChange}
+            />
+          </div>
+        ))}
+      </form>
+    );
+  }
+);
 
 export default DetailsForm;
--- src/pages/EditWarehousePage/EditWarehousePage.jsx
+++ src/pages/EditWarehousePage/EditWarehousePage.jsx
@@ -1,6 +1,8 @@
 import "./EditWarehousePage.scss";
 import { useParams } from "react-router";
 import DetailsForm from "../../components/DetailsForm/DetailsForm";
+import { useRef } from "react";
+import axios from "axios";
 
 const EditWarehousePage = ({ warehouseData }) => {
   const { id } = useParams();
@@ -8,7 +10,38 @@ const EditWarehousePage = ({ warehouseData }) => {
   if (!warehouseToEdit) {
     return <div>warehouse with set id not found</div>;
   }
-  const onSubmit = () => {};
+
+  const warehouseFormRef = useRef();
+  const contactFormRef = useRef();
+
+  const handleSave = async () => {
+    const warehouseFormData = new FormData(warehouseFormRef.current);
+    const contactFormData = new FormData(contactFormRef.current);
+
+    const updatedData = {
+      id: warehouseToEdit.id,
+      warehouse_name: warehouseFormData.get("warehouse_name"),
+      address: warehouseFormData.get("address"),
+      city: warehouseFormData.get("city"),
+      country: warehouseFormData.get("country"),
+      contact_name: contactFormData.get("contact_name"),
+      contact_position: contactFormData.get("contact_position"),
+      contact_phone: contactFormData.get("contact_phone"),
+      contact_email: contactFormData.get("contact_email"),
+    };
+    console.log(updatedData);
+
+    try {
+      const response = await axios.put(`/warehouses/${id}/edit`, updatedData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <main className="main-container">
       <div className="main-container__header">
@@ -21,16 +54,32 @@ const EditWarehousePage = ({ warehouseData }) => {
         </h1>
       </div>
       <hr className="main-container__divider" />
-      <DetailsForm warehouesToEdit={warehouseToEdit} formType="warehouse" />
-      <hr className="main-container__divider" />
-      <DetailsForm warehouesToEdit={warehouseToEdit} formType="contact" />
+
+      <div className="two-forms">
+        <DetailsForm
+          className="two-forms__first"
+          warehouesToEdit={warehouseToEdit}
+          formType="warehouse"
+          ref={warehouseFormRef}
+        />
+        <hr className="two-forms__divider" />
+        <DetailsForm
+          className="two-forms__second"
+          warehouesToEdit={warehouseToEdit}
+          formType="contact"
+          ref={contactFormRef}
+        />
+      </div>
       <div className="whitespace"></div>
 
       <div className="main-container__footer">
         <button className="font-H3-label main-container__footer-cancel">
           Cancel
         </button>
-        <button className="font-H3-label main-container__footer-save">
+        <button
+          className="font-H3-label main-container__footer-save"
+          onClick={handleSave}
+        >
           Save
         </button>
       </div>
