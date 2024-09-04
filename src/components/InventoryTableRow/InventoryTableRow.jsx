c3c2696bb28c91e248709d1dbbdfcf18cf4437f1
farid.ansari <farid.ansari@gmail.com>
2024-09-04 11:07:29 -0700

    fixed a bug where navigation links would not work when the component was in specific warehouse inventory mode

--- src/components/InventoryTableRow/InventoryTableRow.jsx
+++ src/components/InventoryTableRow/InventoryTableRow.jsx
@@ -2,15 +2,20 @@ import "./InventoryTableRow.scss";
 import { NavLink } from "react-router-dom";
 
 const InventoryTableRow = ({ inventory, warehouse_filtered }) => {
-  let wareHouseDiv;
+  let wareHouseDiv = <></>;
+  let parentPathPrefix = "";
   if (warehouse_filtered !== true) {
-     wareHouseDiv = <>      <div className="InventoryTableRow__main__info__col__field">
-     <div className="InventoryTableRow__main__info__col__field__label font-H4-TableHeader">WAREHOUSE</div>
-     <div className="InventoryTableRow__main__info__col__field__data font-P2-BodyMedium">{inventory.warehouse_id}</div>
-   </div>
-     </>;
-  }else{
-    const wareHouseDiv = <></>;
+    wareHouseDiv = (
+      <>
+        {" "}
+        <div className="InventoryTableRow__main__info__col__field">
+          <div className="InventoryTableRow__main__info__col__field__label font-H4-TableHeader">WAREHOUSE</div>
+          <div className="InventoryTableRow__main__info__col__field__data font-P2-BodyMedium">{inventory.warehouse_id}</div>
+        </div>
+      </>
+    );
+  } else {
+    parentPathPrefix = "../inventory/";
   }
   return (
     <>
@@ -21,7 +26,7 @@ const InventoryTableRow = ({ inventory, warehouse_filtered }) => {
             <div className="InventoryTableRow__main__info__col">
               <div className="InventoryTableRow__main__info__col__field">
                 <div className="InventoryTableRow__main__info__col__field__label font-H4-TableHeader">INVENTORY ITEM</div>
-                <NavLink to={inventory.id + "/"} className="InventoryTableRow__main__info__col__field__label__link">
+                <NavLink to={parentPathPrefix + inventory.id + "/"} className="InventoryTableRow__main__info__col__field__label__link">
                   <div className="InventoryTableRow__main__info__col__field__data InventoryTableRow__main__info__col__field__data--link font-H3-label">{inventory.item_name}</div>
                   <img src="/src/assets/images/Icons/chevron_right-24px.svg" alt="link icon" className="InventoryTableRow__main__info__col__field__label__link__icon" />
                 </NavLink>
@@ -40,20 +45,16 @@ const InventoryTableRow = ({ inventory, warehouse_filtered }) => {
                 <div className="InventoryTableRow__main__info__col__field__label font-H4-TableHeader">QTY</div>
                 <div className="InventoryTableRow__main__info__col__field__data font-P2-BodyMedium">{inventory.quantity}</div>
               </div>
-              {/* <div className="InventoryTableRow__main__info__col__field">
-                <div className="InventoryTableRow__main__info__col__field__label font-H4-TableHeader">WAREHOUSE</div>
-                <div className="InventoryTableRow__main__info__col__field__data font-P2-BodyMedium">{inventory.warehouse_id}</div>
-              </div> */}
               {wareHouseDiv}
             </div>
           </td>
 
           <td className="InventoryTableRow__actionContainer">
             <div className="InventoryTableRow__actionContainer__icons">
-              <NavLink to={inventory.id + "/delete/"}>
+              <NavLink to={parentPathPrefix + inventory.id + "/delete/"}>
                 <img className="InventoryTableRow__actionContainer__icons__delete" src="/src/assets/images/Icons/delete_outline-24px.svg" alt="delete icon" />
               </NavLink>
-              <NavLink to={inventory.id + "/edit/"}>
+              <NavLink to={parentPathPrefix + inventory.id + "/edit/"}>
                 <img src="/src/assets/images/Icons/edit-24px.svg" alt="edit icon" />
               </NavLink>
             </div>
