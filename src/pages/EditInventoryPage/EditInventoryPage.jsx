943f83d7dc34d86853676aaae860157f12b0e0c4
Nicole Liu <109707278+nliub@users.noreply.github.com>
2024-09-14 12:41:01 -0700

    fixed headers

--- src/pages/EditInventoryPage/EditInventoryPage.jsx
+++ src/pages/EditInventoryPage/EditInventoryPage.jsx
@@ -5,8 +5,7 @@ import HeaderComponent from "../../components/Header/HeaderComponent";
 import FooterComponent from "../../components/Footer/FooterComponent";
 import axios from "axios";
 
-
-const EditInventoryPage = ({apiURL}) => {
+const EditInventoryPage = ({ apiURL }) => {
   const { id } = useParams();
 
   //Grab the inventory data specific to an id
@@ -15,17 +14,17 @@ const EditInventoryPage = ({apiURL}) => {
   const [warehouses, setWarehouses] = useState([]);
   const navigate = useNavigate();
 
-  useEffect(()=>{
+  useEffect(() => {
     const getSingleInventory = async () => {
-      try{
+      try {
         const response = await axios.get(`${apiURL}/inventory/${id}`);
         setInventoryToEdit(response.data);
-      }catch (error) {
+      } catch (error) {
         console.error(error);
       }
     };
     const getWarehouses = async () => {
-      try{
+      try {
         const response = await axios.get(`${apiURL}/warehouses`);
         setWarehouses(response.data);
       } catch (error) {
@@ -41,11 +40,13 @@ const EditInventoryPage = ({apiURL}) => {
 
   const handleCancel = () => {
     navigate("/inventory");
-  }
- 
+  };
+
   const handleSave = async () => {
     const inventoryFormData = new FormData(inventoryFormRef.current);
-    const itemAvailabilityFormData = new FormData(itemAvailabilityFormRef.current);
+    const itemAvailabilityFormData = new FormData(
+      itemAvailabilityFormRef.current
+    );
 
     const updatedData = {
       id: inventoryToEdit.id,
@@ -53,73 +54,81 @@ const EditInventoryPage = ({apiURL}) => {
       description: inventoryFormData.get("description"),
       category: inventoryFormData.get("category"),
       quantity: Number(itemAvailabilityFormData.get("quantity")),
-      warehouse_id: Number(itemAvailabilityFormData.get("warehouse"))
+      warehouse_id: Number(itemAvailabilityFormData.get("warehouse")),
     };
 
-    try{
+    try {
       const response = await axios.put(
         `${apiURL}/inventory/${id}`,
-        updatedData,
+        updatedData
       );
-      navigate('/inventory');
-    }catch (error) {
+      navigate("/inventory");
+    } catch (error) {
       console.error(error);
     }
   };
 
-  if (!inventoryToEdit){
+  if (!inventoryToEdit) {
     return <div>Loading....</div>;
   }
   return (
     <>
-    <HeaderComponent />
-  <main className="main-container">
-    <div className="main-container__header">
-      <img
-        src="/src/assets/images/Icons/arrow_back-24px.svg"
-        alt="search icon"
-      />
-      <h1 className="font-H1-PageHeader main-container__header-title">
-        Edit Inventory
-      </h1>
-    </div>
-    <hr className="main-container__divider" />
+      <HeaderComponent />
+      <div className="InventoryListPage">
+        <div className="InventoryListPage__left">
+          <div className="AddInventoryPage-spacer"></div>
+        </div>
+        <main className="main-container">
+          <div className="main-container__header">
+            <img
+              src="/src/assets/images/Icons/arrow_back-24px.svg"
+              alt="search icon"
+            />
+            <h1 className="font-H1-PageHeader main-container__header-title">
+              Edit Inventory
+            </h1>
+          </div>
+          <hr className="main-container__divider" />
 
-    <div className="two-forms">
-      <InventoryEdit
-        className="two-forms__first"
-        inventoryToEdit={inventoryToEdit}
-        formType="inventory"
-        ref={inventoryFormRef}
-      />
-      <hr className="two-forms__divider" />
-      <InventoryEdit
-        className="two-forms__second"
-        inventoryToEdit={inventoryToEdit}
-        formType="item availability"
-        ref={itemAvailabilityFormRef}
-        warehouses={warehouses}
-      />        
-    </div>
-    <div className="whitespace"></div>
+          <div className="two-forms">
+            <InventoryEdit
+              className="two-forms__first"
+              inventoryToEdit={inventoryToEdit}
+              formType="inventory"
+              ref={inventoryFormRef}
+            />
+            <hr className="two-forms__divider" />
+            <InventoryEdit
+              className="two-forms__second"
+              inventoryToEdit={inventoryToEdit}
+              formType="item availability"
+              ref={itemAvailabilityFormRef}
+              warehouses={warehouses}
+            />
+          </div>
+          <div className="whitespace"></div>
 
-    <div className="main-container__footer">
-      <button
-        className="font-H3-label main-container__footer-cancel"
-        onClick={handleCancel}
-      >
-        Cancel
-      </button>
-      <button
-        className="font-H3-label main-container__footer-save"
-        onClick={handleSave}
-      >
-        Save
-      </button>
-    </div>
-  </main>
-  <FooterComponent />
-  </>
+          <div className="main-container__footer">
+            <button
+              className="font-H3-label main-container__footer-cancel"
+              onClick={handleCancel}
+            >
+              Cancel
+            </button>
+            <button
+              className="font-H3-label main-container__footer-save"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </div>
+        </main>
+        <div className="InventoryListPage__right">
+          <div className="AddInventoryPage-spacer"></div>
+        </div>
+      </div>
+      <FooterComponent />
+    </>
   );
 };
 
