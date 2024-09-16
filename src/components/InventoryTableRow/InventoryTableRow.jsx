a357e3ce95256b41cfc515ef032b411015f44497
Nicole Liu <109707278+nliub@users.noreply.github.com>
2024-09-15 18:23:36 -0700

    fixed home route links

--- src/components/InventoryTableRow/InventoryTableRow.jsx
+++ src/components/InventoryTableRow/InventoryTableRow.jsx
@@ -1,12 +1,21 @@
 import { useState } from "react";
 import "./InventoryTableRow.scss";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import DeleteInventoryComponent from "../DeleteInventoryComponent/DeleteInventoryComponent";
 import axios from "axios";
 
-const InventoryTableRow = ({ inventory, warehouse_filtered, colSizes, arrayIndex, setInventoryData, inventoryData, apiURL }) => {
+const InventoryTableRow = ({
+  inventory,
+  warehouse_filtered,
+  colSizes,
+  arrayIndex,
+  setInventoryData,
+  inventoryData,
+  apiURL,
+}) => {
   const [isModalOpen, SetIsModalOpen] = useState(false);
   const [selectInventoryId, setSelectInventoryId] = useState(null);
+  const location = useLocation();
 
   const handleDeleteClick = (id) => {
     setSelectInventoryId(id);
@@ -20,7 +29,9 @@ const InventoryTableRow = ({ inventory, warehouse_filtered, colSizes, arrayIndex
   const handleDeleteConfirm = async () => {
     try {
       await axios.delete(`${apiURL}/inventory/${selectInventoryId}`);
-      setInventoryData(inventoryData.filter((inventory) => inventory.id !== selectInventoryId));
+      setInventoryData(
+        inventoryData.filter((inventory) => inventory.id !== selectInventoryId)
+      );
       SetIsModalOpen(false);
     } catch (error) {
       console.error("Failed to delete the inventory item: ", error);
@@ -30,19 +41,26 @@ const InventoryTableRow = ({ inventory, warehouse_filtered, colSizes, arrayIndex
   const inventoryItem = inventory.item_name;
 
   let wareHouseDiv = <></>;
-  let parentPathPrefix = "";
+
   let stockTickerClass = "InventoryTableRow-stockTicker";
   let dividerClass = "InventoryTableRow__divider";
 
   if (warehouse_filtered !== true) {
     wareHouseDiv = (
       <>
-        <div className="InventoryTableRow__main__info__col__field__label font-H4-TableHeader">WAREHOUSE</div>
-        <div className="InventoryTableRow__main__info__col__field__data font-P2-BodyMedium">{inventory.warehouse_name}</div>
+        <div className="InventoryTableRow__main__info__col__field__label font-H4-TableHeader">
+          WAREHOUSE
+        </div>
+        <div className="InventoryTableRow__main__info__col__field__data font-P2-BodyMedium">
+          {inventory.warehouse_name}
+        </div>
       </>
     );
-  } else {
-    parentPathPrefix = "../inventory/";
+  }
+
+  let parentPathPrefix = "/Inventory/";
+  if (location.pathname.toLowerCase().endsWith("/inventory")) {
+    parentPathPrefix = "";
   }
 
   if (inventory.status.toUpperCase() === "OUT OF STOCK") {
@@ -59,35 +77,76 @@ const InventoryTableRow = ({ inventory, warehouse_filtered, colSizes, arrayIndex
         <div className="InventoryTableRow__main">
           <div className="InventoryTableRow__main__info">
             <div className="InventoryTableRow__main__info__col">
-              <div className="InventoryTableRow__main__info__col__field" style={{ width: colSizes[0] }}>
-                <div className="InventoryTableRow__main__info__col__field__label font-H4-TableHeader">INVENTORY ITEM</div>
-                <NavLink to={parentPathPrefix + inventory.id + "/"} className="InventoryTableRow__main__info__col__field__label__link">
-                  <div className="InventoryTableRow__main__info__col__field__data InventoryTableRow__main__info__col__field__data--link font-H3-label">{inventory.item_name}</div>
-                  <img src="/src/assets/images/Icons/chevron_right-24px.svg" alt="link icon" className="InventoryTableRow__main__info__col__field__label__link__icon" />
+              <div
+                className="InventoryTableRow__main__info__col__field"
+                style={{ width: colSizes[0] }}
+              >
+                <div className="InventoryTableRow__main__info__col__field__label font-H4-TableHeader">
+                  INVENTORY ITEM
+                </div>
+                <NavLink
+                  to={parentPathPrefix + inventory.id + "/"}
+                  className="InventoryTableRow__main__info__col__field__label__link"
+                >
+                  <div className="InventoryTableRow__main__info__col__field__data InventoryTableRow__main__info__col__field__data--link font-H3-label">
+                    {inventory.item_name}
+                  </div>
+                  <img
+                    src="/src/assets/images/Icons/chevron_right-24px.svg"
+                    alt="link icon"
+                    className="InventoryTableRow__main__info__col__field__label__link__icon"
+                  />
                 </NavLink>
               </div>
-              <div className="InventoryTableRow__main__info__col__field" style={{ width: colSizes[1] }}>
-                <div className="InventoryTableRow__main__info__col__field__label font-H4-TableHeader">CATERGORY</div>
-                <div className="InventoryTableRow__main__info__col__field__data font-P2-BodyMedium">{inventory.category}</div>
+              <div
+                className="InventoryTableRow__main__info__col__field"
+                style={{ width: colSizes[1] }}
+              >
+                <div className="InventoryTableRow__main__info__col__field__label font-H4-TableHeader">
+                  CATERGORY
+                </div>
+                <div className="InventoryTableRow__main__info__col__field__data font-P2-BodyMedium">
+                  {inventory.category}
+                </div>
               </div>
             </div>
             <div className="InventoryTableRow__main__info__col">
-              <div className="InventoryTableRow__main__info__col__field" style={{ width: colSizes[2] }}>
-                <div className="InventoryTableRow__main__info__col__field__label font-H4-TableHeader">STATUS</div>
+              <div
+                className="InventoryTableRow__main__info__col__field"
+                style={{ width: colSizes[2] }}
+              >
+                <div className="InventoryTableRow__main__info__col__field__label font-H4-TableHeader">
+                  STATUS
+                </div>
                 <div className={stockTickerClass}>
-                  <div className="InventoryTableRow__main__info__col__field__data font-P3-BodySmall">{inventory.status.toUpperCase()}</div>
+                  <div className="InventoryTableRow__main__info__col__field__data font-P3-BodySmall">
+                    {inventory.status.toUpperCase()}
+                  </div>
                 </div>
               </div>
-              <div className="InventoryTableRow__main__info__col__field" style={{ width: colSizes[3] }}>
-                <div className="InventoryTableRow__main__info__col__field__label font-H4-TableHeader">QTY</div>
-                <div className="InventoryTableRow__main__info__col__field__data font-P2-BodyMedium">{inventory.quantity}</div>
+              <div
+                className="InventoryTableRow__main__info__col__field"
+                style={{ width: colSizes[3] }}
+              >
+                <div className="InventoryTableRow__main__info__col__field__label font-H4-TableHeader">
+                  QTY
+                </div>
+                <div className="InventoryTableRow__main__info__col__field__data font-P2-BodyMedium">
+                  {inventory.quantity}
+                </div>
               </div>
-              <div className="InventoryTableRow__main__info__col__field" style={{ width: colSizes[4] }}>
+              <div
+                className="InventoryTableRow__main__info__col__field"
+                style={{ width: colSizes[4] }}
+              >
                 {wareHouseDiv}
               </div>
             </div>
             <div className="InventoryTableRow__main__info__col InventoryTableRow__main__info__col--actions">
-              <div className="InventoryTableRow__main__info__col__field" style={{ width: colSizes[5] }}>
+              <div
+                className="InventoryTableRow__main__info__col__field"
+                style={{ width: colSizes[5] }}
+              >
                 <div className="InventoryTableRow__main__info__col__field__icons">
                   <img
                     onClick={() => {
@@ -98,7 +157,10 @@ const InventoryTableRow = ({ inventory, warehouse_filtered, colSizes, arrayIndex
                     alt="delete icon"
                   />
                   <NavLink to={parentPathPrefix + inventory.id + "/edit/"}>
-                    <img src="/src/assets/images/Icons/edit-24px.svg" alt="edit icon" />
+                    <img
+                      src="/src/assets/images/Icons/edit-24px.svg"
+                      alt="edit icon"
+                    />
                   </NavLink>
                 </div>
               </div>
@@ -106,7 +168,12 @@ const InventoryTableRow = ({ inventory, warehouse_filtered, colSizes, arrayIndex
           </div>
         </div>
       </div>
-      <DeleteInventoryComponent isOpen={isModalOpen} onClose={handleCloseModal} onDelete={handleDeleteConfirm} inventoryItem={inventoryItem} />
+      <DeleteInventoryComponent
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        onDelete={handleDeleteConfirm}
+        inventoryItem={inventoryItem}
+      />
     </>
   );
 };
